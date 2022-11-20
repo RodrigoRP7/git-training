83edf2a3f134cde0215054233d45162bd68260e4
RodrigoRP7 <rodrigoruanoperez@gmail.com>
2022-11-20 13:44:50 +0100

    MENSAJE ULTIMO

---
+++ execute.js
@@ -0,0 +1,37 @@
+const { exec } = require("child_process");
+
+exec("git add .", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+exec("git commit -m \"MENSAJE ULTIMO\"", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+exec("git push", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
